94287c322dc73addddb70c9c2eaf9f37c6b426da
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-03-14 23:44:38 +0000

    Create a Simple Class and Object

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person person = new Person();
+        person.name = "ahmad";
+        person.age = 23;
+
+        person.introduce();
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public String name  ;
+    public int age;
+
+    public boolean introduce(){
+        System.out.println("Hi, my name is "+ name + " and I am "+ age +"years old.");
+
+
+        return false;
+    }
+
+}
